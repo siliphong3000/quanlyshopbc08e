203d6d376e82e5554bf2014cb72c05316b8d619a
Tấn Phát Lê <vuabanhmi951@gmail.com>
2021-07-14 21:07:58 +0700

    version2_vietChucNangRenderProduct

--- controller/index.js
+++ controller/index.js
@@ -1,1 +1,27 @@
 console.log('hello cybersoft')
+
+var arrProduct = [
+    {id: 1, name: "Iphone X", price: 1000, img: 'https://cdn.tgdd.vn/Products/Images/42/114115/iphone-x-64gb-hh-600x600.jpg'},
+    {id: 1, name: "Iphone X", price: 1000, img: 'https://cdn.tgdd.vn/Products/Images/42/114115/iphone-x-64gb-hh-600x600.jpg'},
+    {id: 1, name: "Iphone X", price: 1000, img: 'https://cdn.tgdd.vn/Products/Images/42/114115/iphone-x-64gb-hh-600x600.jpg'}
+]
+
+function renderProduct(){
+    var content = '';
+    for (var i = 0; i < arrProduct.length; i++) {
+        let product = arrProduct[i];
+        content += `
+            <div class = "col-4">
+                <div class = "card">
+                <img src = "${product.img}" alt = "..."/>
+                    <div class = "card-body">
+                    <p>${product.price}</p>
+                    <button>Mua Hàng</button>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+    document.querySelector('#arrProduct').innerHTML = content;
+}
+renderProduct();
